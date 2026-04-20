58d32b128b103663fc060fcd4e6b182b13c0b5f9
emil:io <emilio.mateo.rg@gmail.com>
2026-04-20 17:10:32 -0600

    conexion con bd
    hola

---
+++ proyectoWebIIP1_8H1/backend/src/controllers/productos.controller.js
@@ -0,0 +1,16 @@
+const db = require('../config/db');
+
+const getProductos = (req, res) =>{
+    const sql = 'SELECT * FROM productos';
+    console.log("hola")
+    db.query(sql, (error, resultados) => {
+        if (error){
+            return res.status(500).json({error: 'Error al obtener productos'});
+        }
+        res.json(resultados);
+    });
+};
+
+module.exports = {
+    getProductos
+};
